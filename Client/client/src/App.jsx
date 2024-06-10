bcb284dadd235b012846d2d1619a4b21c09ea44b
hezethegamer <hezethegamer01@gmail.com>
2024-06-10 06:05:30 -0700

    hh

--- Client/client/src/App.jsx
+++ Client/client/src/App.jsx
@@ -9,7 +9,13 @@ import SignUpForm from './components/SignUpForm';
 import ForgotPassword from './components/ForgotPassword'; // Import ForgotPassword component
 
 import './App.css';
+function App() {
+  const [message, setMessage] = useState('');
 
+  useEffect(() => {
+    fetch('http://localhost:4000/api/message')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
 const App = () => {
   return (
     <Router>
